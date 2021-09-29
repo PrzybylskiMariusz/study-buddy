40c41ea027860587f30eeb14a7c3c8db0a00f59c
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2021-09-29 17:59:11 +0200

    First components

---
+++ src/data/users.js
@@ -0,0 +1,37 @@
+export const users = [
+  {
+    name: 'Adam Romański',
+    attendance: '38%',
+    average: '2.3',
+  },
+  {
+    name: 'Krzysztof Batko',
+    attendance: '38%',
+    average: '3.5',
+  },
+  {
+    name: 'Patrycja Gonciarz',
+    attendance: '38%',
+    average: '4.3',
+  },
+  {
+    name: 'Olga Hahn',
+    attendance: '38%',
+    average: '3.2',
+  },
+  {
+    name: 'Paweł Andrzejewski',
+    attendance: '38%',
+    average: '2.3',
+  },
+  {
+    name: 'Szymon Knuth',
+    attendance: '38%',
+    average: '4.1',
+  },
+  {
+    name: 'Krystian Foczpaniak',
+    attendance: '38%',
+    average: '2.5',
+  },
+];
