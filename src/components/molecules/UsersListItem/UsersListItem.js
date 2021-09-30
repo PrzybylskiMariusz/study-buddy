d5d7845d89b23dc14c287138e5a15c2062510c10
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2021-09-30 18:10:12 +0200

    Styling and refactor

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../../atoms/Button/Button';
-import { Wrapper } from './UsersListItem.styles';
+import { StyledAverage, StyledInfo, Wrapper } from './UsersListItem.styles';
 
-const UsersListItem = ({ userData: { average, name, attendance } }) => (
+const showIndex = (index) => alert(`This is student #${index + 1}`);
+
+const UsersListItem = ({ index, userData: { average, name, attendance } }) => (
   <Wrapper>
-    <div>{average}</div>
-    <div>
-      <p>{name}</p>
-      <p>{attendance}</p>
-    </div>
-    <Button />
+    <StyledAverage value={average}>{average}</StyledAverage>
+    <StyledInfo>
+      <p>
+        {name}
+        <Button onClick={() => showIndex(index)} />
+      </p>
+      <p> attendance: {attendance}</p>
+    </StyledInfo>
   </Wrapper>
 );
 
