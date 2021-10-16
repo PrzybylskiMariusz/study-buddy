e6bd364b765a97d9454ceb14cedd2ec0f69e4cf5
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2021-10-16 20:51:11 +0200

    Create basic react router and form

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '../../atoms/Button/Button';
+import DeleteButton from 'components/atoms/DeleteButton/DeleteButton';
 import { StyledAverage, StyledInfo, Wrapper } from './UsersListItem.styles';
 
-const UsersListItem = ({ deleteUser, userData: { average, name, attendance } }) => (
+const UsersListItem = ({ deleteUser, userData: { average, name, attendance = '0%' } }) => (
   <Wrapper>
     <StyledAverage value={average}>{average}</StyledAverage>
     <StyledInfo>
       <p>
         {name}
-        <Button onClick={() => deleteUser(name)} />
+        <DeleteButton onClick={() => deleteUser(name)} />
       </p>
-      <p> attendance: {attendance}</p>
+      <p>attendance: {attendance}</p>
     </StyledInfo>
   </Wrapper>
 );
 
 UsersListItem.propTypes = {
   userData: PropTypes.shape({
-    average: PropTypes.string,
-    name: PropTypes.string,
+    average: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
     attendance: PropTypes.string,
   }),
 };
