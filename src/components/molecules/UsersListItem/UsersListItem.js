7e6159f0311f43e9969250170b4b7178e028fefb
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2021-10-16 18:54:44 +0200

    Create State in UsersList

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -3,15 +3,13 @@ import PropTypes from 'prop-types';
 import Button from '../../atoms/Button/Button';
 import { StyledAverage, StyledInfo, Wrapper } from './UsersListItem.styles';
 
-const showIndex = (index) => alert(`This is student #${index + 1}`);
-
-const UsersListItem = ({ index, userData: { average, name, attendance } }) => (
+const UsersListItem = ({ deleteUser, userData: { average, name, attendance } }) => (
   <Wrapper>
     <StyledAverage value={average}>{average}</StyledAverage>
     <StyledInfo>
       <p>
         {name}
-        <Button onClick={() => showIndex(index)} />
+        <Button onClick={() => deleteUser(name)} />
       </p>
       <p> attendance: {attendance}</p>
     </StyledInfo>
